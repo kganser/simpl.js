259d0a76ef3be5f133498521437182f9b7f8db2b
Klaus Ganser <kganser@gmail.com>
2015-04-21 19:01:51 -0500

    Fix array header serialization

--- modules/http.js
+++ modules/http.js
@@ -19,9 +19,9 @@ simpl.add('http', function(modules) {
         headers['Content-Length'] = body.length;
       if (chunk && headers['Transfer-Encoding'] === undefined)
         headers['Transfer-Encoding'] = 'chunked';
-      pre += 'HTTP/1.1 '+self.statusMessage(status)+'\r\n'+Object.keys(headers).map(function flatten(name) {
+      pre += 'HTTP/1.1 '+self.statusMessage(status)+'\r\n'+Object.keys(headers).map(function(name) {
         var value = headers[name];
-        return value ? Array.isArray(value) ? value.map(flatten).join('') : name+': '+value+'\r\n' : '';
+        return value ? Array.isArray(value) ? value.map(function(value) { return name+': '+value+'\r\n'; }).join('') : name+': '+value+'\r\n' : '';
       }).join('')+'\r\n';
     }
     
