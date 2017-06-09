c814a7cb0ce6048d85f5630d6cc8178f58935f1a
Klaus Ganser <kganser@gmail.com>
2017-06-09 16:46:59 -0500

    Wrap to 120 chars

--- apps/database-admin.js
+++ apps/database-admin.js
@@ -33,25 +33,79 @@ function(modules) {
         {title: (db ? db+' - ' : '')+'Database Admin'},
         {meta: {charset: 'utf-8'}},
         {style: html.css({
-          body: {font: '13px sans-serif', webkitTextSizeAdjust: 'none'},
-          svg: {width: '1.2em', height: '1.2em', margin: '0 .5em', verticalAlign: 'bottom', fill: '#aaa'},
-          'form input': {outline: 'none', border: 'solid 1px #ececec', height: '23px', padding: '0 5px'},
-          'form button': {outline: 'none', border: 'none', background: '#3c3', color: 'white', cursor: 'pointer', padding: '0 8px', height: '25px'},
-          'button.delete': {background: 'transparent', color: '#aaa', padding: '0 5px', height: '17px'},
-          'button.delete:hover': {background: '#c5201c', color: 'white'},
-          '#icons': {display: 'none'},
-          '.databases': {listStyle: 'none', padding: 0, lineHeight: 1.5},
-          '.databases a, a.home': {textDecoration: 'none', color: '#1c00cf'},
-          'a.home:before': {content: '""', display: 'inline-block', border: '0 solid transparent', borderWidth: '.35em .6em .35em 0', borderRightColor: '#aaa'},
-          'a:hover svg, a:focus svg, a:active svg': {fill: '#5a5a5a'},
-          'a.home:hover:before, a.home:focus:before, a.home:active:before': {borderRightColor: '#5a5a5a'}
+          body: {
+            font: '13px sans-serif',
+            webkitTextSizeAdjust: 'none'
+          },
+          svg: {
+            width: '1.2em',
+            height: '1.2em',
+            margin: '0 .5em',
+            verticalAlign: 'bottom',
+            fill: '#aaa'
+          },
+          'form input': {
+            outline: 'none',
+            border: 'solid 1px #ececec',
+            height: '23px',
+            padding: '0 5px'
+          },
+          'form button': {
+            outline: 'none',
+            border: 'none',
+            background: '#3c3',
+            color: 'white',
+            cursor: 'pointer',
+            padding: '0 8px',
+            height: '25px'
+          },
+          'button.delete': {
+            background: 'transparent',
+            color: '#aaa',
+            padding: '0 5px',
+            height: '17px'
+          },
+          'button.delete:hover': {
+            background: '#c5201c',
+            color: 'white'
+          },
+          '#icons': {
+            display: 'none'
+          },
+          '.databases': {
+            listStyle: 'none',
+            padding: 0,
+            lineHeight: 1.5
+          },
+          '.databases a, a.home': {
+            textDecoration: 'none',
+            color: '#1c00cf'
+          },
+          'a.home:before': {
+            content: '""',
+            display: 'inline-block',
+            border: '0 solid transparent',
+            borderWidth: '.35em .6em .35em 0',
+            borderRightColor: '#aaa'
+          },
+          'a:hover svg, a:focus svg, a:active svg': {
+            fill: '#5a5a5a'
+          },
+          'a.home:hover:before, a.home:focus:before, a.home:active:before': {
+            borderRightColor: '#5a5a5a'
+          }
         })},
         db && {link: {rel: 'stylesheet', href: '/static/jsonv.css'}}
       ]},
       {body: [
         {svg: {id: 'icons', children: [
-          {symbol: {id: 'icon-database', viewBox: '0 0 20 20', children: {path: {d: 'M16.726,12.641c-0.843,1.363-3.535,2.361-6.726,2.361c-3.191,0-5.883-0.998-6.727-2.361C3.095,12.351,3,12.506,3,12.648c0,0.144,0,2.002,0,2.002C3,16.59,6.134,18.6,10,18.6s7-2.01,7-3.949c0,0,0-1.858,0-2.002C17,12.506,16.904,12.351,16.726,12.641zM16.737,7.525c-0.83,1.205-3.532,2.09-6.737,2.09c-3.205,0-5.908-0.885-6.738-2.09C3.091,7.277,3,7.412,3,7.523c0,0.113,0,2.357,0,2.357c0,1.762,3.134,3.189,7,3.189s7-1.428,7-3.189c0,0,0-2.244,0-2.357C17,7.412,16.908,7.277,16.737,7.525zM10,1C6.134,1,3,2.18,3,3.633v1.26c0,1.541,3.134,2.791,7,2.791s7-1.25,7-2.791v-1.26C17,2.18,13.866,1,10,1z'}}}}
-        ]}},
+          {symbol: {id: 'icon-database', viewBox: '0 0 20 20', children: {path: {d: 'M16.726,12.641c-0.843,1.363-3.535'+
+            ',2.361-6.726,2.361c-3.191,0-5.883-0.998-6.727-2.361C3.095,12.351,3,12.506,3,12.648c0,0.144,0,2.002,0,2.00'+
+            '2C3,16.59,6.134,18.6,10,18.6s7-2.01,7-3.949c0,0,0-1.858,0-2.002C17,12.506,16.904,12.351,16.726,12.641zM16'+
+            '.737,7.525c-0.83,1.205-3.532,2.09-6.737,2.09c-3.205,0-5.908-0.885-6.738-2.09C3.091,7.277,3,7.412,3,7.523c'+
+            '0,0.113,0,2.357,0,2.357c0,1.762,3.134,3.189,7,3.189s7-1.428,7-3.189c0,0,0-2.244,0-2.357C17,7.412,16.908,7'+
+            '.277,16.737,7.525zM10,1C6.134,1,3,2.18,3,3.633v1.26c0,1.541,3.134,2.791,7,2.791s7-1.25,7-2.791v-1.26C17,2'+
+            '.18,13.866,1,10,1z'}}}}]}},
         body
       ]}
     ]);
@@ -93,15 +147,22 @@ function(modules) {
                 {input: {type: 'hidden', name: 'token', value: csrf}},
                 {input: {type: 'hidden', name: 'action', value: 'delete'}},
                 {button: {type: 'submit', class: 'delete', name: 'name', value: name, children: '×'}},
-                {a: {href: '/'+encodeURIComponent(name), children: [{svg: [{use: {'xlink:href': '#icon-database'}}]}, name]}}
+                {a: {href: '/'+encodeURIComponent(name), children: [
+                  {svg: [{use: {'xlink:href': '#icon-database'}}]},
+                  name
+                ]}}
               ]}}
             ]};
           })}},
-          {form: {method: 'post', onsubmit: 'if (!this.name.value) { alert("Please specify a database name."); this.name.focus(); return false; }', children: [
-            {input: {placeholder: 'New Database', name: 'name'}},
-            {input: {type: 'hidden', name: 'token', value: csrf}},
-            {button: {type: 'submit', name: 'action', value: 'add', children: 'Add'}}
-          ]}}
+          {form: {
+            method: 'post',
+            onsubmit: 'if (!this.name.value) { alert("Database name required"); this.name.focus(); return false; }',
+            children: [
+              {input: {placeholder: 'New Database', name: 'name'}},
+              {input: {type: 'hidden', name: 'token', value: csrf}},
+              {button: {type: 'submit', name: 'action', value: 'add', children: 'Add'}}
+            ]
+          }}
         ]), {
           'Content-Type': 'text/html',
           'X-CSRF-Token': csrf
@@ -266,7 +327,8 @@ function(modules) {
                   }
                   if (callback) callback(error, value);
                 };
-                request.open(method, location.pathname+'/'+path.map(encodeURIComponent).join('/')+(method == 'get' && value != null ? '?after='+encodeURIComponent(value) : ''));
+                request.open(method, location.pathname+'/'+path.map(encodeURIComponent).join('/')+
+                  (method == 'get' && value != null ? '?after='+encodeURIComponent(value) : ''));
                 request.setRequestHeader('Accept', 'application/json');
                 request.setRequestHeader('Content-Type', 'application/json');
                 request.setRequestHeader('Authorization', token);
