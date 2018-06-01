a6efdecd905520b5f3967a8c7f87bf6c567cab18
kganser <kganser@gmail.com>
2018-06-01 11:24:39 -0500

    Use chrome debugger to run tests

--- test.js
+++ test.js
@@ -1,47 +1,119 @@
 function(modules) {
 
-  var host = '//localhost:'+config.apiPort,
-      ws;
-      
-  modules.http.serve({port: config.port}, function(req, res) {
-    if (req.path != '/' || ws) return res.generic(404);
-    
-    var close = function() {
-      if (!ws) return;
-      ws.close();
-      ws = null;
-      res.end('</testsuite>');
-    };
+  var string = modules.string,
+      host = 'http://localhost:'+config.apiPort,
+      tests = [], log = [], screenshot;
 
-    setTimeout(close, 10000);
+  var run = fetch('http://localhost:'+config.debuggerPort+'/json').then(function(response) {
+    return response.json();
+  }).then(function(pages) {
+    log.push('> pages: '+JSON.stringify(pages));
+    var page = pages.find(function(page) {
+      return page.url == 'chrome-extension://kdkheiopehanfbajkkopimfppjmclgbp/simpl.html'
+    });
+    if (!page) return log.push('> debug page not found');
 
-    res.send('<?xml version="1.0" encoding="UTF-8"?><testsuite tests="92">', 'xml');
-
-    fetch('http:'+host+'/token').then(function(res) { return res.text(); }).then(function(token) {
-      ws = new WebSocket('ws:'+host+'/connect?token='+token);
-      ws.onopen = function() {
-        fetch('http:'+host+'/action', {
-          method: 'POST',
-          body: JSON.stringify({
-            command: 'restart',
-            app: 'Unit Tests',
-            version: 1,
-            token: token
-          })
+    return new Promise(function(resolve) {
+      var socket = new WebSocket(page.webSocketDebuggerUrl),
+          callbacks = {},
+          id = 1;
+      var send = function(method, params) {
+        var message = JSON.stringify({id: id, method: method, params: params});
+        log.push('> '+message);
+        socket.send(message);
+        return new Promise(function(resolve) {
+          callbacks[id++] = resolve;
         });
       };
-      ws.onmessage = function(e) {
-        try { var message = JSON.parse(e.data); } catch (e) { return; }
-        var data = message.data || {};
-        if (message.event != 'log' || data.app != 'Unit Tests') return;
-        var line = String((data.message || [])[0]),
-            type = line[0],
-            detail = line.substr(2);
-        if (type != '✓' && type != '✗') return;
-        res.send('<testcase name="'+detail+'"'+(type == '✓' ? '/>' : '><failure message="test failed"/></testcase>'));
-        if (!detail.indexOf('tests complete')) close();
+      var wait = function(method) {
+        log.push('> wait for '+method);
+        return new Promise(function(resolve) {
+          callbacks[method] = resolve;
+        });
       };
-      ws.onclose = close;
+      var assert = function(test, description) {
+        log.push('> '+(test ? '✓' : '✗')+' '+description);
+        tests.push({name: description, pass: test});
+      };
+      socket.onopen = function() {
+        log.push('> debugger connection opened');
+        Promise.all([
+          send('Runtime.enable'),
+          send('Page.enable'),
+          send('Network.enable')
+        ]).then(function() {
+          return send('Page.navigate', {url: host});
+        }).then(function() {
+          return Promise.all([,
+            send('Emulation.setDeviceMetricsOverride', {width: 600, height: 400, deviceScaleFactor: 0, mobile: false}),
+            send('Emulation.setVisibleSize', {width: 600, height: 400}),
+            wait('Page.loadEventFired')
+          ]);
+        }).then(function() {
+          return send('Runtime.evaluate', {expression: 'location.href'});
+        }).then(function(result) {
+          assert(result.result.value == host+'/', 'Page loaded');
+          return send('Runtime.evaluate', {expression: "Array.from(document.querySelectorAll('nav li .name')).map(elem => elem.textContent).join();"});
+        }).then(function(result) {
+          assert(result.result.value == 'Database Admin,Hello World,Simple Login,Unit Tests,Web Server,test,crypto,database,docs,email,html,http,parser,socket,string,system,websocket', 'Modules loaded');
+          return send('Runtime.evaluate', {expression: "document.querySelector('nav li:nth-child(4) .run').click();"});
+        }).then(function(result) {
+          assert(result.result.type == 'undefined', 'Ran Unit Tests app');
+          return function read() {
+            return wait('Network.webSocketFrameReceived').then(function(params) {
+              var message = '';
+              try {
+                var payload = JSON.parse(params.response.payloadData),
+                    data = payload.data;
+                if (payload.event == 'log' && data.app == 'Unit Tests') {
+                  message = data.message[0];
+                  var type = message[0];
+                  if (type == '✓' || type == '✗') {
+                    assert(type == '✓', 'Unit tests:'+message.substr(1));
+                  }
+                }
+              } catch (e) {}
+              if (message.indexOf('tests complete') < 0)
+                return read();
+            });
+          }();
+        }).then(function() {
+          return send('Page.captureScreenshot');
+        }).then(function(result) {
+          screenshot = string.base64ToBuffer(result.data).buffer;
+          resolve();
+        });
+      };
+      socket.onmessage = function(e) {
+        log.push(e.data.substr(0, 1000));
+        try { var data = JSON.parse(e.data); } catch (e) { return; }
+        var key = data.id || data.method;
+        var callback = callbacks[key];
+        if (callback) {
+          delete callbacks[key];
+          callback(data.result || data.params);
+        }
+      };
     });
+  });
+
+  modules.http.serve({port: config.port}, function(req, res) {
+
+    if (req.path == '/results')
+      return run.then(function() {
+        res.end('<?xml version="1.0" encoding="UTF-8"?><testsuite tests="95">'+tests.map(function(test) {
+          return '<testcase name="'+test.name+'"'+(test.pass ? '/>' : '><failure message="test failed"/></testcase>');
+        }).join('')+'</testsuite>', {'Content-Type': 'text/xml; charset=utf-8'});
+      });
+    if (req.path == '/log')
+      return run.then(function() {
+        res.end(log.join('\n\n'), {'Content-Type': 'text/plain; charset=utf-8'});
+      });
+    if (req.path == '/screenshot')
+      return run.then(function() {
+        if (screenshot) return res.end(screenshot, 'png');
+        res.generic(404);
+      });
+    res.generic(404);
   });
 }
--- test.sh
+++ test.sh
@@ -2,17 +2,22 @@
 
 if [ ! -x $1 ]; then echo 'bad executable'; exit 1; fi
 
-$1 --disable-gpu --enable-logging=stderr --port=8123 --debug &
+$1 --remote-debugging-port=8122 --enable-logging=stderr --port=8123 &
 PID=$!
-sleep 1
-TOKEN=`curl -s localhost:8123/token`
+while test -z "$TOKEN"; do
+  sleep 1
+  TOKEN=`curl -s localhost:8123/token`
+done
 curl localhost:8123/restore -d "scope=full&token=$TOKEN" -so /dev/null
 curl localhost:8123/apps/test/1?token=$TOKEN -X PUT --data-binary @test.js -so /dev/null
 curl localhost:8123/apps/test/1/dependencies?token=$TOKEN -d '{"name":"http","version":0}' -so /dev/null
-curl localhost:8123/apps/test/1/config?token=$TOKEN -X PUT -d '{"port":8124,"apiPort":8123}' -so /dev/null
+curl localhost:8123/apps/test/1/dependencies?token=$TOKEN -d '{"name":"string","version":0}' -so /dev/null
+curl localhost:8123/apps/test/1/config?token=$TOKEN -X PUT -d '{"port":8124,"apiPort":8123,"debuggerPort":8122}' -so /dev/null
 curl localhost:8123/action -d "{\"command\":\"run\",\"app\":\"test\",\"version\":1,\"token\":\"$TOKEN\"}" -so /dev/null
 sleep 1
 mkdir -p reports/test.js
-curl localhost:8124 -so reports/test.js/junit.xml
+curl localhost:8124/results -so reports/test.js/junit.xml
+curl localhost:8124/log -so reports/test.js/log.txt
+curl localhost:8124/screenshot -so reports/test.js/screenshot.png
 kill $PID
 wait $PID
