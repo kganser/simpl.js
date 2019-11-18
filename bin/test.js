ccb3488b7b1ec75fab54dd8abee1362eff60b29e
kganser <kganser@gmail.com>
2019-11-17 18:45:06 -0600

    Add code coverage

--- bin/test.js
+++ bin/test.js
@@ -27,17 +27,57 @@ const app = spawn(baseDir + '/build/' + (
 });
 
 [app.stdout, app.stderr].forEach(input => {
+  const tag = input == app.stdout ? '[CHROME OUT] ' : '[CHROME ERR] ';
   readline.createInterface({input}).on('line', line => {
-    if (line) log.push((input == app.stdout ? '[CHROME OUT] ' : '[CHROME ERR] ') + line);
+    if (line) log.push(tag + line);
   });
 });
 
 const tests = [];
 const log = [];
-const listeners = {};
 const timeout = 10000;
+const scripts = [{url: baseUrl + '/console.js'}];
 
-// TODO: code coverage
+// https://github.com/GoogleChrome/puppeteer/blob/561c99/lib/Coverage.js#L269
+function convertToDisjointRanges(nestedRanges) {
+  const points = [];
+  for (const range of nestedRanges) {
+    points.push({ offset: range.startOffset, type: 0, range });
+    points.push({ offset: range.endOffset, type: 1, range });
+  }
+  points.sort((a, b) => {
+    if (a.offset !== b.offset)
+      return a.offset - b.offset;
+    if (a.type !== b.type)
+      return b.type - a.type;
+    const aLength = a.range.endOffset - a.range.startOffset;
+    const bLength = b.range.endOffset - b.range.startOffset;
+    if (a.type === 0)
+      return bLength - aLength;
+    return aLength - bLength;
+  });
+
+  const hitCountStack = [];
+  const results = [];
+  let lastOffset = 0;
+  for (const point of points) {
+    if (hitCountStack.length && lastOffset < point.offset && hitCountStack[hitCountStack.length - 1] > 0) {
+      const lastResult = results.length ? results[results.length - 1] : null;
+      if (lastResult && lastResult.end === lastOffset)
+        lastResult.end = point.offset;
+      else
+        results.push({start: lastOffset, end: point.offset});
+    }
+    lastOffset = point.offset;
+    if (point.type === 0)
+      hitCountStack.push(point.range.count);
+    else
+      hitCountStack.pop();
+  }
+  return results.filter(range => range.end - range.start > 1);
+}
+
+// TODO: background.js code coverage
 // TODO: handle early app crash, ws disconnect
 
 app.on('error', console.error);
@@ -46,143 +86,13 @@ const assert = (pass, name, detail, images) => {
   console.log(pass ? '\x1b[32m✓\x1b[0m ' + name : '\x1b[31m✗\x1b[0m ' + name + '\n  ' + detail);
   log.push('[TEST] ' + (pass ? '✓ ' : '✗ ') + name + '\n  ' + detail);
   tests.push({name, pass: !!pass, detail, images});
+  return pass;
 };
 
-(async () => {
-
-  const token = await (async retries => {
-    while (retries--) {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      try {
-        const response = await fetch(baseUrl + '/token', {timeout});
-        if (response.ok) return response.text();
-      } catch (e) {
-        // continue
-      }
-    }
-  })(5);
-
-  assert(token, 'Get token', token);
-  if (!token) return;
-
-  const page = await fetch('http://localhost:' + debuggerPort + '/json')
-    .then(data => data.json())
-    .then(pages => pages.find(page => page.url == 'chrome-extension://kdkheiopehanfbajkkopimfppjmclgbp/simpl.html'));
-
-  assert(page, 'Get page from debugger');
-  if (!page) return;
-
-  const request = (path, options) =>
-    fetch(baseUrl + path, {headers: {Authorization: 'Bearer ' + token}, timeout, ...options});
-
-  await request('/restore', {method: 'post', body: 'scope=full'}).then(response => {
-    assert(response.ok, 'Reset workspace', 'Returned ' + response.status);
-  });
-
-  const socket = new WebSocket(page.webSocketDebuggerUrl, {perMessageDeflate: false});
-
+const connect = async url => {
+  const listeners = {};
   let id = 1;
-  const send = (method, params) => {
-    const message = JSON.stringify({id, method, params});
-    log.push('[TEST] ' + message);
-    socket.send(message);
-    return new Promise((resolve, reject) => {
-      const i = id++;
-      listeners[i] = resolve;
-      setTimeout(() => {
-        if (!listeners[i]) return;
-        delete listeners[i];
-        reject('Timed out waiting for response to ' + method);
-      }, timeout);
-    });
-  };
-
-  const next = (method, test) => {
-    log.push('[TEST] wait for ' + method);
-    return new Promise((resolve, reject) => {
-      listeners[method] = resolve;
-      setTimeout(() => {
-        if (!listeners[method]) return;
-        // delete listeners[method]; // prevents reject() from propagating?
-        reject('Timed out waiting for ' + method);
-      }, timeout);
-    }).then(result =>
-      !test || test(result) ? result : next(method, test)
-    );
-  };
-
-  const load = url => {
-    return next('Network.requestWillBeSent', result =>
-      result.request.url.split('?')[0] == baseUrl + url
-    ).then(req =>
-      next('Network.loadingFinished', result => result.requestId == req.requestId)
-    );
-  };
-
-  const snapshot = async name => {
-    const id = name.toLowerCase();
-    const {data} = await send('Page.captureScreenshot');
-
-    const lastFile = 'snapshots/' + id + '.' + platform + '.png';
-    const nextFile = 'snapshots/' + id + '.' + platform + '.new.png';
-
-    const [last, next] = await Promise.all([
-      new Promise((resolve, reject) =>
-        fs.createReadStream(baseDir + '/test/' + lastFile)
-          .on('error', err => {
-            if (err.code == 'ENOENT') {
-              resolve();
-            } else {
-              reject('Error reading ' + lastFile + ': ' + err.code);
-            }
-          })
-          .pipe(new PNG())
-          .on('parsed', function() { resolve(this); })
-      ),
-      new Promise((resolve, reject) =>
-        new PNG().parse(
-          Buffer.from(data, 'base64'),
-          (err, data) => {
-            if (err) return reject('Error parsing ' + id + ' screenshot');
-            resolve(data);
-          }
-        )
-      )
-    ]);
-    
-    if (last) {
-      const description = name + ' matches snapshot';
-      if (
-        last.width == next.width &&
-        last.height == next.height &&
-        last.data.every((pixel, i) => pixel == next.data[i])
-      ) {
-        assert(true, description, null, [lastFile]);
-      } else {
-        assert(false, description, 'Comparing ' + lastFile + ' to ' + nextFile, [lastFile, nextFile]);
-        return new Promise((resolve, reject) =>
-          next.pack()
-            .pipe(fs.createWriteStream(baseDir + '/test/' + nextFile))
-            .on('finish', resolve)
-            .on('error', err => reject('Error writing to ' + nextFile + ': ' + err.code))
-        );
-      }
-    } else if (next) {
-      await new Promise((resolve, reject) =>
-        fs.mkdir(baseDir + '/test/snapshots', {recursive: true}, err => {
-          if (!err || err.code == 'EEXIST') return resolve();
-          reject('Error creating screenshots dir: ' + err.code);
-        }));
-      return new Promise((resolve, reject) =>
-        next.pack()
-          .pipe(fs.createWriteStream(baseDir + '/test/' + lastFile))
-          .on('error', err => reject('Error writing to ' + lastFile + ': ' + err.code))
-          .on('finish', resolve)
-      ).then(() => {
-        assert(true, name + ' snapshot created', null, [lastFile]);
-      });
-    }
-  };
+  const socket = new WebSocket(url, {perMessageDeflate: false});
 
   socket.on('unexpected-response', (req, res) => {
     let body = '';
@@ -200,7 +110,7 @@ const assert = (pass, name, detail, images) => {
       const key = id || method;
       const listener = listeners[key];
       if (listener) {
-        delete listeners[key];
+        if (key == id) delete listeners[key];
         listener(result || params);
       }
     } catch (e) {
@@ -212,9 +122,149 @@ const assert = (pass, name, detail, images) => {
   // TODO: socket.on('close')
 
   await new Promise((resolve, reject) => {
-    setTimeout(() => reject('Timed out connecting to ' + page.webSocketDebuggerUrl), timeout);
+    setTimeout(() => reject('Timed out connecting to ' + url), timeout);
     socket.once('open', resolve);
   });
+
+  const self = {
+    load: function(url) {
+      return self.next('Network.requestWillBeSent', result =>
+        result.request.url.split('?')[0] == baseUrl + url
+      ).then(req =>
+        self.next('Network.loadingFinished', result => result.requestId == req.requestId)
+      );
+    },
+    next: function(method, test) {
+      log.push('[TEST] wait for ' + method);
+      return new Promise((resolve, reject) => {
+        self.subscribe(method, resolve);
+        setTimeout(() => {
+          if (!listeners[method]) return;
+          // delete listeners[method]; // prevents reject() from propagating?
+          reject('Timed out waiting for ' + method);
+        }, timeout);
+      }).then(result =>
+        !test || test(result) ? result : self.next(method, test)
+      );
+    },
+    send: function(method, params) {
+      const message = JSON.stringify({id, method, params});
+      log.push('[TEST] ' + message);
+      socket.send(message);
+      return new Promise((resolve, reject) => {
+        const i = id++;
+        listeners[i] = resolve;
+        setTimeout(() => {
+          if (!listeners[i]) return;
+          delete listeners[i];
+          reject('Timed out waiting for response to ' + method);
+        }, timeout);
+      });
+    },
+    snapshot: async function(name) {
+      const id = name.toLowerCase();
+      const {data} = await self.send('Page.captureScreenshot');
+
+      const lastFile = 'snapshots/' + id + '.' + platform + '.png';
+      const nextFile = 'snapshots/' + id + '.' + platform + '.new.png';
+
+      const [last, next] = await Promise.all([
+        new Promise((resolve, reject) =>
+          fs.createReadStream(baseDir + '/test/' + lastFile)
+            .on('error', err => {
+              if (err.code == 'ENOENT') {
+                resolve();
+              } else {
+                reject('Error reading ' + lastFile + ': ' + err.code);
+              }
+            })
+            .pipe(new PNG())
+            .on('parsed', function() { resolve(this); })
+        ),
+        new Promise((resolve, reject) =>
+          new PNG().parse(
+            Buffer.from(data, 'base64'),
+            (err, data) => {
+              if (err) return reject('Error parsing ' + id + ' screenshot');
+              resolve(data);
+            }
+          )
+        )
+      ]);
+
+      if (last) {
+        const description = name + ' matches snapshot';
+        if (
+          last.width == next.width &&
+          last.height == next.height &&
+          last.data.every((pixel, i) => pixel == next.data[i])
+        ) {
+          assert(true, description, null, [lastFile]);
+        } else {
+          assert(false, description, 'Comparing ' + lastFile + ' to ' + nextFile, [lastFile, nextFile]);
+          return new Promise((resolve, reject) =>
+            next.pack()
+              .pipe(fs.createWriteStream(baseDir + '/test/' + nextFile))
+              .on('finish', resolve)
+              .on('error', err => reject('Error writing to ' + nextFile + ': ' + err.code))
+          );
+        }
+      } else if (next) {
+        await new Promise((resolve, reject) =>
+          fs.mkdir(baseDir + '/test/snapshots', {recursive: true}, err => {
+            if (!err || err.code == 'EEXIST') return resolve();
+            reject('Error creating screenshots dir: ' + err.code);
+          }));
+        return new Promise((resolve, reject) =>
+          next.pack()
+            .pipe(fs.createWriteStream(baseDir + '/test/' + lastFile))
+            .on('error', err => reject('Error writing to ' + lastFile + ': ' + err.code))
+            .on('finish', resolve)
+        ).then(() => {
+          assert(true, name + ' snapshot created', null, [lastFile]);
+        });
+      }
+    },
+    subscribe: function(method, listener) {
+      listeners[method] = listener;
+    },
+  };
+
+  return self;
+};
+
+(async () => {
+
+  const token = await (async retries => {
+    while (retries--) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      try {
+        const response = await fetch(baseUrl + '/token', {timeout});
+        if (response.ok) return response.text();
+      } catch (e) {
+        // continue
+      }
+    }
+  })(5);
+
+  if (!assert(token, 'Get token', token))
+    return;
+
+  const page = await fetch('http://localhost:' + debuggerPort + '/json')
+    .then(data => data.json())
+    .then(pages => pages.find(page => page.url == 'chrome-extension://kdkheiopehanfbajkkopimfppjmclgbp/simpl.html'));
+
+  if (!assert(page, 'Get page from debugger'))
+    return;
+
+  const request = (path, options) =>
+    fetch(baseUrl + path, {headers: {Authorization: 'Bearer ' + token}, timeout, ...options});
+
+  await request('/restore', {method: 'post', body: 'scope=full'}).then(response => {
+    assert(response.ok, 'Reset workspace', 'Returned ' + response.status);
+  });
+
+  const {send, subscribe, next, load, snapshot} = await connect(page.webSocketDebuggerUrl);
 
   assert(true, 'Open debugger connection');
 
@@ -222,8 +272,18 @@ const assert = (pass, name, detail, images) => {
   await Promise.all([
     send('Runtime.enable'),
     send('Page.enable'),
-    send('Network.enable')
+    send('Network.enable'),
+    send('Profiler.enable'),
+    send('Debugger.enable')
   ]);
+  send('Profiler.startPreciseCoverage', {callCount: false, detailed: true});
+  subscribe('Debugger.scriptParsed', async ({scriptId, url}) => {
+    const script = scripts.find(script => script.url == url);
+    if (script) {
+      script.id = scriptId;
+      script.source = (await send('Debugger.getScriptSource', {scriptId})).scriptSource;
+    }
+  });
   await Promise.all([
     send('Page.navigate', {url: baseUrl}),
     send('Emulation.setDeviceMetricsOverride', {width, height, deviceScaleFactor: 0, mobile: false}),
@@ -353,6 +413,22 @@ const assert = (pass, name, detail, images) => {
   } catch (e) {
     assert(false, 'Config snapshot', e);
   }
+
+  const [profile] = await Promise.all([
+    send('Profiler.takePreciseCoverage'),
+    send('Profiler.stopPreciseCoverage')
+  ]);
+
+  profile.result.forEach(({scriptId, functions}) => {
+    const script = scripts.find(script => script.id == scriptId);
+    if (script) script.ranges = convertToDisjointRanges(
+      functions.reduce((ranges, fn) => [
+        ...ranges,
+        ...fn.ranges
+      ], [])
+    );
+  });
+
 })()
 .catch(e => {
   assert(false, e && e.message || e, e && e.stack);
@@ -374,7 +450,7 @@ const assert = (pass, name, detail, images) => {
   const escape = value =>
     String(value).replace(/&/g, '&amp;').replace(/"/g, '&quot;');
   const markup = node => {
-    if (typeof node == 'string') return node;
+    if (typeof node == 'string' || typeof node == 'number') return node;
     if (!Array.isArray(node)) return '';
     const [tag, ...children] = node;
     if (Array.isArray(tag)) return node.map(markup).join('');
@@ -404,7 +480,9 @@ const assert = (pass, name, detail, images) => {
         '.test:before': {display: 'inline-block', marginLeft: '-1.5em', width: '1.5em'},
         '.pass:before': {content: '"✓"', color: 'green'},
         '.fail:before': {content: '"✗"', color: 'red'},
-        '.test img': {width: '100%', maxWidth: '400px', margin: '10px 0'}
+        '.test img': {width: '100%', maxWidth: '400px', margin: '10px 0'},
+        '.coverage table': {borderCollapse: 'collapse', font: '.6em monospace', whiteSpace: 'pre-wrap'},
+        '.coverage del': {background: '#fdb8c0', textDecoration: 'none'}
       })],
       ['h1', 'Simpl.js'],
       ['h2', 'Tests'],
@@ -420,7 +498,66 @@ const assert = (pass, name, detail, images) => {
       ['details',
         ['summary', 'Log'],
         ['pre', escape(log.join('\n\n'))]
-      ]
+      ],
+      ['h2', 'Code Coverage'],
+      ...scripts.map(({url, source, ranges}) => {
+        // coverage spans
+        const spans = [];
+        let position = 0;
+        ranges.forEach(range => {
+          if (range.start > position)
+            spans.push({text: source.substring(position, range.start), covered: false});
+          spans.push({text: source.substring(range.start, range.end), covered: true});
+          position = range.end;
+        });
+        if (position < source.length)
+          spans.push({text: source.substr(position), covered: false});
+        // lines
+        const lines = [];
+        let line = [];
+        spans.forEach(({text, covered}) => {
+          const [first, ...rest] = text.split(/\r?\n/g);
+          if (first) line.push({text: first, covered});
+          rest.forEach(text => {
+            if (line.length) lines.push(line);
+            line = text ? [{text, covered}] : [];
+          });
+        });
+        if (line.length) lines.push(line);
+        // blocks
+        const blocks = [];
+        let block;
+        lines.forEach((spans, i) => {
+          const covered = spans.length == 1 && spans[0].covered;
+          const line = {number: i + 1, spans};
+          if (block && block.covered == covered) {
+            block.lines.push(line);
+          } else {
+            if (block) blocks.push(block);
+            block = {lines: [line], covered};
+          }
+        });
+        if (block) blocks.push(block);
+
+        const coveredLines = lines.reduce((total, spans) =>
+          spans.length == 1 && spans[0].covered ? total + 1 : total, 0);
+
+        return ['details', {class: 'coverage'},
+          ['summary', url + ' - ' + Math.floor(coveredLines / lines.length * 100) + '% lines'],
+          ['table', ...blocks.map(({covered, lines}) =>
+            ['tbody',
+              covered
+                ? ['tr', ['td', {colspan: 2}, '...']]
+                : lines.map(({number, spans}) => ['tr',
+                    ['td', number],
+                    ['td', ...spans.map(({covered, text}) =>
+                      covered ? escape(text) : ['del', escape(text)]
+                    )]
+                  ])
+            ]
+          )]
+        ];
+      })
     ]),
     err => {
       if (err) console.error('Error writing to results.html: ' + err.code);
